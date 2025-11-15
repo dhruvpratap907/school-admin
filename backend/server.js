793f7088cf1a8dd474c6febc5e193161347983b3
Dhruv Pratap Singh <dhruvpratap907@gmail.com>
2025-11-16 00:55:55 +0530

    new setting

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,5 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Serve frontend files
-app.use(express.static(path.join(__dirname))); // index.html, script.js are in root
-
-// For any other route, serve index.html
+app.use(express.static(__dirname)); // everything in backend
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
